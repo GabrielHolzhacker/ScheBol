e9ff9a46c75859c298845ca6ea93d42978dd13f4
GabrielHolzhacker <gabrielbh@al.insper.edu.br>
2025-04-28 11:44:06 -0300

    terceiro código

--- funcoes.py
+++ funcoes.py
@@ -12,3 +12,11 @@ def guardar_dado (dados_rolados, dados_guardados, num):
         if i != num:
             dados_rolados_novo.append(dados_rolados[i])
     return [dados_rolados_novo, dados_guardados]
+
+def remover_dado(dados_rolados, dados_guardados, num):
+    dados_rolados.append(dados_guardados[num])
+    dados_guardados_novo= []
+    for i in range (len(dados_guardados)):
+        if i != num:
+            dados_guardados_novo.append(dados_guardados[i])
+    return [dados_rolados, dados_guardados_novo]

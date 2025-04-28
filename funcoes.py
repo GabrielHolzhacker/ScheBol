8d4de051e6b00aae68abeeb65f087e8aea627dfe
GabrielHolzhacker <gabrielbh@al.insper.edu.br>
2025-04-28 11:36:31 -0300

    segundo código

--- funcoes.py
+++ funcoes.py
@@ -4,3 +4,11 @@ def rolar_dados (num):
     for numero in range(num):
         lista.append(randint(1, 6))
     return lista
+
+def guardar_dado (dados_rolados, dados_guardados, num):
+    dados_guardados.append(dados_rolados[num])
+    dados_rolados_novo = []
+    for i in range (len(dados_rolados)):
+        if i != num:
+            dados_rolados_novo.append(dados_rolados[i])
+    return [dados_rolados_novo, dados_guardados]
